7c5c5506fe8009190aa45f9ef8887857cff1b390
Арсений <seha2005@bk.ru>
2024-06-05 20:28:28 +0300

    fix bugs

--- pacman.py
+++ pacman.py
@@ -687,7 +687,7 @@ class Ghost:
         if self.x_pos < -30:
             self.x_pos = 900
         elif self.x_pos > 900:
-            self.x_pos - 30
+            self.x_pos = - 30
         return self.x_pos, self.y_pos, self.direction
 
 
